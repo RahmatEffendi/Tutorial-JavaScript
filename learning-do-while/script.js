fe0a25027a15874f0b52ff8564da05c8eeaddc1a
RahmatEffendi <rahmat480@gmail.com>
2018-08-21 11:44:37 +0700

    upload do-while and for

---
+++ learning-do-while/script.js
@@ -0,0 +1,29 @@
+/**
+ * Do While
+ * 
+ */
+
+/**
+ * While
+ */
+
+//x = 0;
+//while (x <= 10) {
+//    document.writeln("Number is " + x + '<br>');
+//    x++;
+//}
+
+/**
+ * Do - While
+ * 
+ */
+
+//x = 0;
+//do {
+//    document.writeln("Number is " + x + '<br>');
+//    x++;
+//}
+
+//while (x < 8) {
+//    document.write("Loop Stop");
+//}
---
+++ learning-for/script.js
@@ -0,0 +1,10 @@
+/**
+ * For Loop
+ * 
+ * 
+ */
+
+var x;
+for (x = 0; x <= 10; x++) {
+    document.writeln("Number " + x + '<br>');
+}
