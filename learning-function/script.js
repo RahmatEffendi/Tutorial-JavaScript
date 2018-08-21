3d4113b947c98ccbd639957f22135544d0462c90
RahmatEffendi <rahmat480@gmail.com>
2018-08-21 16:43:43 +0700

    upload new update for function folder

--- learning-function/script.js
+++ learning-function/script.js
@@ -3,6 +3,26 @@
  * 
  * 
  */
-function sayHello() {
+
+//Function without parameter
+/**function sayHello() {
     document.write("Hello, Guys");
+}*/
+
+//function with parameter
+/**function sayHello(name, age) {
+    document.write("Hello " + name + ". Your Age is " + age);
+}*/
+
+//function with return value
+function concatenate(first, last) {
+    var fullname;
+    fullname = first + last;
+    return fullname;
+}
+
+function sayHello() {
+    var result;
+    result = concatenate('Rahmat', ' Effendi');
+    document.write("Hello " + result);
 }
