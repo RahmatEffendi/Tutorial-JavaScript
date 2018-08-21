a2c9c22cdbcf16089b1ed57ad556b986ac5d53d5
RahmatEffendi <rahmat480@gmail.com>
2018-08-21 13:52:43 +0700

    upload learning control loop

---
+++ learning-control-loop/script.js
@@ -0,0 +1,28 @@
+/**
+ * Control Loop (Break and Countinue)
+ * 
+ */
+
+//var x = 1;
+//while (x <= 10) {
+//    if (x == 5) {
+//        break;
+//    }
+//    x = x + 1;
+//    document.writeln(x + "<br>");
+//}
+
+/**
+ * Control Loop (Countinue)
+ * 
+ * 
+ */
+var x = 1;
+while (x < 20) {
+    x = x + 1;
+
+    if (x == 5) {
+        continue;
+    }
+    document.write(x + "<br>");
+}
