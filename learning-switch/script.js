3e29ea6c804e444cb43e6aee738fd7e7947e509f
RahmatEffendi <rahmat480@gmail.com>
2018-08-21 10:49:13 +0700

    upload learning switch

---
+++ learning-switch/script.js
@@ -0,0 +1,39 @@
+/**
+ * Switch - Case
+ * 
+ * 
+ */
+
+var x = prompt("Enter Day : ");
+switch (x) {
+    case 'senin':
+        document.writeln('The Day is ' + x);
+        break;
+
+    case 'selasa':
+        document.writeln("The day is " + x);
+        break;
+
+    case 'rabu':
+        document.writeln("The day is " + x);
+        break;
+
+    case 'kamis':
+        document.writeln("The day is " + x);
+        break;
+
+    case 'jumat':
+        document.writeln("The day is " + x);
+        break;
+
+    case 'sabtu':
+        document.writeln("The day is " + x);
+        break;
+
+    case 'minggu':
+        document.writeln("The day is " + x);
+        break;
+
+    default:
+        document.writeln("Input Day with Bahasa Language and lowcase word");
+}
