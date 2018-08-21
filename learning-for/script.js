cc57da407c3ac8f137ed816d98c9340ed40cdd10
RahmatEffendi <rahmat480@gmail.com>
2018-08-21 12:48:22 +0700

    update script js in learning for

--- learning-for/script.js
+++ learning-for/script.js
@@ -4,7 +4,18 @@
  * 
  */
 
-var x;
-for (x = 0; x <= 10; x++) {
-    document.writeln("Number " + x + '<br>');
+//var x;
+//for (x = 0; x <= 10; x++) {
+//    document.writeln("Number " + x + '<br>');
+//}
+
+/**
+ * For In
+ * 
+ * 
+ */
+
+var aProperty;
+for (aProperty in navigator) {
+    document.writeln(aProperty + '<br>');
 }
