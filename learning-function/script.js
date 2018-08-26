8d6a0c025fcb82b0981035c848f19c3eafbddb8f
RahmatEffendi <rahmat480@gmail.com>
2018-08-26 16:37:57 +0700

    update new source

--- learning-function/script.js
+++ learning-function/script.js
@@ -15,7 +15,7 @@
 }*/
 
 //function with return value
-function concatenate(first, last) {
+/** function concatenate(first, last) {
     var fullname;
     fullname = first + last;
     return fullname;
@@ -25,4 +25,24 @@ function sayHello() {
     var result;
     result = concatenate('Rahmat', ' Effendi');
     document.write("Hello " + result);
+} */
+
+//Function with constructor function
+/**var func = new Function("x", "y", "return x*y");
+
+function seconFunc() {
+    var result;
+    result = func(12, 93);
+    document.write(result);
+}*/
+
+//function with function literal
+var func = function(x, y) {
+    return x * y;
+}
+
+function seconFunc() {
+    var result;
+    result = func(20, 30);
+    document.write(result);
 }
