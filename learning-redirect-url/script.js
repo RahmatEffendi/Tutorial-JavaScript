66c4b77aa102315e27caa70711b49b31d12f1392
RahmatEffendi <rahmat480@gmail.com>
2018-08-27 09:34:24 +0700

    Upload new source

---
+++ learning-redirect-url/script.js
@@ -0,0 +1,13 @@
+/**
+ * Redirect Url
+ * 
+ * 
+ */
+
+function Redirect() {
+    window.location = "https://amdc.co.id"
+}
+
+//Redirect with time
+document.write("You will redirect in 10 second");
+setTimeout('Redirect()', 10000);
